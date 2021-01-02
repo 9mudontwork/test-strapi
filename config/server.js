ae557b6eb500a6948ba7841e0323d4670d11108a
Muhammad Imoeb <muhammad@diversition.co.th>
2021-01-02 14:29:48 +0700

    ✨ init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '46a6492072b5de5cea46a838392c809b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8cb56efb-e8ee-4bd0-a8e6-893fc2f5d191'
+};
